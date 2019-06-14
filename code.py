8023383b1f391a392540247e2251603d0cbb70a2
Dawson <37384968+DeeJayNZ@users.noreply.github.com>
2019-06-14 12:17:54 +1200

    Add files via upload

--- code.py
+++ code.py
@@ -5,7 +5,7 @@ class canteen_food:#names the class
     _ids = count(0) #sets _ids to count(0)
     
     
-    def __init__(self, name, image, stock, price, description, sold):#this function initialises all the variables you want in your menu, name, stock, price ect.
+    def __init__(self, name, image, stock, price, description, sold):#this function creates all the objects you want in your menu, name, stock, price ect.
         self.id = next(self._ids)
         self.food_name = name
         self.food_image = image
@@ -29,8 +29,8 @@ def index():#this function will attatch the decorators above
 @route("/food")
 @view("food")
 def menu_page():
-    data = dict (food_list = canteen_test)
-    return data
+    data = dict (food_list = canteen_test)# this makes a dictionary with all my test data
+    return data # this returns the data
 
 @route ("/tandc")
 @view("tandc")
@@ -38,17 +38,17 @@ def terms_page():
     pass
         
         
-@route("/success/<food_id>")
+@route("/success/<food_id>")#passes food_id
 @view("success")
 def success_page(food_id):
-    food_id = int(food_id)
-    found_food = None
-    for food in canteen_test:
-        if food.id == food_id:
-            found_food = food
-    data = dict(food = found_food)
-    found_food.food_stock -= 1
-    found_food.food_sold += 1
+    food_id = int(food_id) # this sets food_id to the number of food_id
+    found_food = None # sets found_food to none
+    for food in canteen_test: #says for each item of food in canteen test DO
+        if food.id == food_id: # if the two food id s match.
+            found_food = food #set found_food to food
+    data = dict(food = found_food) # sets data to the dictionary with all the test data
+    found_food.food_stock -= 1 # when a food item is purchased it takes one off stock
+    found_food.food_sold += 1 #also adds one to total food sold whoch I display on the table webpage
 
     return data    
 
@@ -66,7 +66,7 @@ def restock_page(food_id):
     return data
 
 
-@route("/restock-success/<food_id>", method="POST")
+@route("/restock-success/<food_id>", method="POST")# method = POST is the method by which the python server recieves the food_id
 @view("restock-success")
 def restock_success(food_id):
     food_id = int(food_id)
@@ -75,7 +75,7 @@ def restock_success(food_id):
         if food.id == food_id:
             found_food = food
             data = dict(food = found_food)
-            restock = request.forms.get("restock")
+            restock = request.forms.get("restock")# gets the amount to restock off the form on the webpage
             restock = int(restock)
             found_food.food_stock = found_food.food_stock + restock
             
@@ -85,8 +85,8 @@ def table():
     data = dict (food_list = canteen_test)
     return data
 
-@route('/picture/<filename>')
+@route('/picture/<filename>')# this allows my webpage to display saved images, which I have saved under the picture folder.
 def saved_pics(filename):
     return static_file(filename, root = './images')       
         
-run(host = "0.0.0.0", port = 8080, reloader = True, debug = True)
+run(host = "0.0.0.0", port = 8080, reloader = True, debug = True)# this allows you to access the website on chrome, through port 8080
